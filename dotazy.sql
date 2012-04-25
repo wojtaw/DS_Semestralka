a7cdc3c2fe1b0f83f4b3bd692bf1e1c135fb61fa
Jáchym Barvínek <jachymb@gmail.com>
2012-04-25 14:00:09 +0200

    2. dotaz

--- dotazy.sql
+++ dotazy.sql
@@ -1,3 +1,6 @@
 /* tento select přidá k id presentací všechny jejich tagy */
 
-select presentations.id as presentation_id, array(select tags.name from tags,tags_presentations where tags.id = tags_presentations.tag_id and presentations.id = tags_presentations.presentation_id) as tag_array from presentations order by id;
+select presentations.id as presentation_id, array(select tags.name from tags,tags_presentations where tags.id = tags_presentations.tag_id and presentations.id = tags_presentations.presentation_id order by tags.id) as tag_array from presentations order by id;
+
+/* tento select vrátí k presentaci 6 id presentací, které mají shodné tagy a jejich počet. Na 1. řádku je sama presentace s počtem tagů. */
+select nested.presentation_id as np ,count(tp.tag_id) as eqtags from tags_presentations tp, (select * from tags_presentations) as nested where tp.presentation_id=6 and tp.tag_id=nested.tag_id group by np order by eqtags desc;
